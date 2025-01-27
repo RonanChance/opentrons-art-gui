0acd63b4203e3b639b354b5fecd6dd5a02723746
RonanChance <chancesec@proton.me>
2025-01-27 16:31:48 -0500

    Draw by holding mouse button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
 
     let show_outlines = $state(true);
     let current_point = $state({});
+    let isDrawing;
 
     let current_color = $state('Blue');
     const well_colors = {
@@ -158,8 +159,6 @@ for i, point_list in enumerate([blue_points, red_points, yellow_points, green_po
             console.log("Failed to copy:", err);
         }
     }
-
-
 </script>
 
 <article class="prose w-full mx-auto mt-5">
@@ -179,10 +178,21 @@ for i, point_list in enumerate([blue_points, red_points, yellow_points, green_po
     </label>
 </div>
 
-<div class="relative border border-neutral rounded-full mx-auto w-full max-w-[94vw] max-h-[94vw] sm:max-w-[500px] sm:max-h-[500px] aspect-square mb-6" style="width: {(radius_mm + radius_margin_mm) * 16}px; height: {(radius_mm + radius_margin_mm) * 16}px;">
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div 
+    class="relative border border-neutral rounded-full mx-auto w-full max-w-[94vw] max-h-[94vw] sm:max-w-[500px] sm:max-h-[500px] aspect-square mb-6"
+    style="width: {(radius_mm + radius_margin_mm) * 16}px; height: {(radius_mm + radius_margin_mm) * 16}px;"
+    onmousedown={() => isDrawing = true}
+    onmouseup={() => isDrawing = false}
+    onmouseleave={() => isDrawing = false}
+    ontouchstart={() => isDrawing = true}
+    ontouchend={() => isDrawing = false}
+    ontouchcancel={() => isDrawing = false}
+    draggable="false"
+>
     {#each points as { x, y }}
-    <!-- TODO: figure out if w-4 h-4 is a reasonable estimate -->
-        <input type="checkbox" id="dot-{x}-{y}" 
+        <!-- svelte-ignore a11y_mouse_events_have_key_events -->
+        <input type="checkbox" id="dot-{x}-{y}"  
             class="checkbox w-4 h-4 absolute rounded-full [--chkfg:invisible] transition-[box-shadow] duration-300 ease-in-out {point_colors[`${x}, ${y}`] ? 'border-0' : ''} {show_outlines ? '' : 'border-0'}"
             style=" 
                 left: calc(50% + ({x / radius_mm} * 50%) - 8px); 
@@ -190,10 +200,22 @@ for i, point_list in enumerate([blue_points, red_points, yellow_points, green_po
                 background-color: {well_colors[point_colors[`${x}, ${y}`]] || 'transparent'};
                 box-shadow: {point_colors[`${x}, ${y}`] ? `0 0 7px 3px ${well_colors[point_colors[`${x}, ${y}`]]}` : 'none'}
                 "
-            onmouseover={() => current_point = { x, y }}
-            onfocus={() => current_point = { x, y }}
-            onmouseout={() => current_point = {}}
-            onblur={() => current_point = {}}
+            draggable="false"
+            onmouseover={() => {
+                if (isDrawing) {
+                    point_colors[`${x}, ${y}`] = current_color;
+                    points_by_color = groupByColors();
+                }
+            }}
+            ontouchmove={(e) => {
+                e.preventDefault(); // Prevent scrolling while drawing
+                const touch = e.touches[0];
+                const target = document.elementFromPoint(touch.clientX, touch.clientY);
+                if (target && target.id.startsWith("dot-")) {
+                    point_colors[`${x}, ${y}`] = current_color;
+                    points_by_color = groupByColors();
+                }
+            }}
             onclick={() => {
                 if (point_colors[`${x}, ${y}`] === current_color) {
                     delete point_colors[`${x}, ${y}`];
@@ -203,9 +225,10 @@ for i, point_list in enumerate([blue_points, red_points, yellow_points, green_po
                 }
                 points_by_color = groupByColors();
             }}
-            />
+        />
     {/each}
 </div>
+
 
 <div class="flex flex-col px-5 gap-4 w-full max-w-[100vw] sm:max-w-[500px] mx-auto mb-[150px]">
     <div class="flex flex-row w-full gap-6">
@@ -281,8 +304,8 @@ for i, point_list in enumerate([blue_points, red_points, yellow_points, green_po
     </div>
 
     <!-- RESET BUTTON -->
-    <button class="btn btn-neutral btn-sm ml-auto" onclick={resetValues}>
-        <svg class="w-5 h-5" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g fill="none" fill-rule="evenodd" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" transform="translate(2 2)"> <path d="m4.5 1.5c-2.4138473 1.37729434-4 4.02194088-4 7 0 4.418278 3.581722 8 8 8s8-3.581722 8-8-3.581722-8-8-8"></path> <path d="m4.5 5.5v-4h-4"></path> </g> </g></svg>
+    <button class="btn btn-sm ml-auto hover:bg-neutral hover:text-white " onclick={resetValues}>
+        <svg class="w-5 h-5" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" transform="translate(2 2)"> <path d="m4.5 1.5c-2.4138473 1.37729434-4 4.02194088-4 7 0 4.418278 3.581722 8 8 8s8-3.581722 8-8-3.581722-8-8-8"></path> <path d="m4.5 5.5v-4h-4"></path> </g> </g></svg>
         Reset All
     </button>
 
@@ -313,19 +336,16 @@ for i, point_list in enumerate([blue_points, red_points, yellow_points, green_po
             </div>
         </div>
     </div>
-    <div class="collapse collapse-arrow">
-        <input type="checkbox" id="section2" class="toggle-checkbox" />
-        <label for="section2" class="collapse-title text-lg font-medium">Code & License</label>
-        <div class="collapse-content text-sm">
-            <p>This project is <a class="underline" href="https://github.com/RonanChance/opentrons-art-gui/blob/main/LICENSE">MIT Licensed</a> and I would love your help in adding new features!</p>
-            <div class="max-w-[200px] mx-auto pt-4">
-                <a href="https://github.com/RonanChance/opentrons-art-gui" target="_blank" data-value="github" style="border-radius:4px;" class="py-2 px-1 flex justify-center items-center bg-neutral hover:bg-gray-900 text-white transition ease-in duration-100 text-center text-sm font-semibold shadow-md focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="mr-2" viewBox="0 0 1792 1792">
-                    <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z"></path>
-                    </svg>
-                    View on GitHub
-                </a>
-            </div>
+    
+    <div class="text-sm px-4 mt-5">
+        <!-- <p>This project is <a class="underline" href="https://github.com/RonanChance/opentrons-art-gui/blob/main/LICENSE">MIT Licensed</a> and I would love your help in adding new features!</p> -->
+        <div class="max-w-[160px] mx-auto pt-4">
+            <a href="https://github.com/RonanChance/opentrons-art-gui" target="_blank" data-value="github" style="border-radius:2px;" class="py-2 px-1 flex justify-center items-center bg-neutral-200 hover:bg-neutral hover:text-white text-neutral transition ease-in duration-100 text-center text-sm font-semibold shadow-md focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="mr-2" viewBox="0 0 1792 1792">
+                <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z"></path>
+                </svg>
+                View on GitHub
+            </a>
         </div>
     </div>
 </div>
