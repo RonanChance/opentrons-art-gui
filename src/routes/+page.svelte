a407d97c6c73a54a2bb32e3de3c42f1784ae409d
RonanChance <chancesec@proton.me>
2025-06-17 15:39:05 -0400

    Begin work on print time estimation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,6 +36,7 @@
     let title = $state('');
     let author = $state('');
     let QRCode_text = $state('');
+    let estimatedPrintDuration = $state(0);
     
     // IMAGE DATA
     let pixelatedSrc = $state(null);
@@ -503,6 +504,33 @@ def run(protocol):
         pixelatedSrc = canvas.toDataURL();
         imageColors = getPixelHexColors(ctx, canvasSize, canvasSize);
     }
+
+    function formatSeconds(seconds) {
+        let totalDuration = 0;
+        for (const key in points_by_color) {
+            const points = points_by_color[key];
+            let curDuration = 0;
+
+            for (let i = 1; i < points.length; i++) {
+                const [x1, y1] = points[i - 1].point;
+                const [x2, y2] = points[i].point;
+                const dx = x2 - x1;
+                const dy = y2 - y1;
+                // duration: print point
+                curDuration += Math.sqrt(dx * dx + dy * dy) / 5;
+            }
+            totalDuration += curDuration;
+            // duration: switch pipette
+            totalDuration += 12;
+            // duration: refill pipette
+            totalDuration +=  Math.ceil(points_by_color[key].length / 18) * 5;
+        }
+        seconds = totalDuration;
+        
+        const mins = Math.floor(seconds / 60);
+        const secs = Math.floor(seconds % 60);
+        return `${mins}:${secs.toString().padStart(2, '0')}`;
+    }
 </script>
 
 <article class="prose w-full mx-auto mt-5">
@@ -680,6 +708,12 @@ def run(protocol):
             }}
         />
     {/each}
+    {#if Object.keys(point_colors).length > 0}
+        <div class="flex flex-row items-center gap-1 justify-center align-middle absolute bottom-0 left-0 origin-bottom-left opacity-50 tooltip tooltip-bottom" data-tip="Estimated Print Duration" transition:fade={{ duration: 200 }}>
+            <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12ZM3.00683 12C3.00683 16.9668 7.03321 20.9932 12 20.9932C16.9668 20.9932 20.9932 16.9668 20.9932 12C20.9932 7.03321 16.9668 3.00683 12 3.00683C7.03321 3.00683 3.00683 7.03321 3.00683 12Z" fill="#0F0F0F"></path> <path d="M12 5C11.4477 5 11 5.44771 11 6V12.4667C11 12.4667 11 12.7274 11.1267 12.9235C11.2115 13.0898 11.3437 13.2343 11.5174 13.3346L16.1372 16.0019C16.6155 16.278 17.2271 16.1141 17.5032 15.6358C17.7793 15.1575 17.6155 14.5459 17.1372 14.2698L13 11.8812V6C13 5.44772 12.5523 5 12 5Z" fill="#0F0F0F"></path> </g></svg>
+            <div class="">{formatSeconds(estimatedPrintDuration)}</div>
+        </div>
+    {/if}
     {#if Object.keys(point_colors).length > 0 && (grid_style === "Standard" || grid_style === "Image")}
         <div class="absolute bottom-0 right-0 scale-[60%] origin-bottom-right" transition:fade={{ duration: 200 }}>
             <div class="flex w-full justify-center">
